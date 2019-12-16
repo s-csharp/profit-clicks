00a809bcf14deffb0bc2460fcc27c9d157e63f98
Sergey K <s.csharp@rambler.ru>
2019-12-16 15:15:49 +0300

    Добавьте файлы проекта.

---
+++ ProfitClicksHelpers/Helpers/ArrayHelpers.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProfitClicksHelpers.Helpers
+{
+    /// <summary>
+    /// Хелпер для работы с массивами
+    /// </summary>
+    public static class ArrayHelper
+    {
+        /// <summary>
+        /// Проверка существования двух чисел как сумма другого 
+        /// </summary>
+        /// <param name="source">Последовательность чисел</param>
+        /// <param name="number">Число, как сумма двух из последовательности</param>
+        /// <exception cref="ArgumentNullException">Последовательность не определена или пуста</exception>
+        /// <returns>Да / Нет</returns>
+        public static bool ExistNumbersAsSum(int[] source, int number)
+        {
+            if (source == null ||
+                source.Length == 0)
+                throw new ArgumentNullException(nameof(source), "Sequence is null or empty");
+
+            var result = false;
+            var table = new HashSet<int>(source.Length);
+
+            foreach (var item in source)
+            {
+                var value = number - item;
+
+                if (!table.Contains(value))
+                    table.Add(item);
+                else
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Поиск максимального префикса среди последовательности строк
+        /// </summary>
+        /// <param name="source">Последовательность строк</param>
+        /// <exception cref="ArgumentNullException">Последовательность не определена или пуста</exception>
+        /// <returns>Максимальный префикс</returns>
+        public static string MaxPrefix(string[] source)
+        {
+            if (source == null ||
+                source.Length == 0)
+                throw new ArgumentNullException(nameof(source), "Sequence is null or empty");
+
+            var result = new StringBuilder();
+
+            var maxPrefix = source[0]?.Length ?? 0;
+
+            foreach (var item in source)
+            {
+                if (item == null ||
+                    item.Length == 0)
+                {
+                    maxPrefix = 0;
+                    break;
+                }
+                else if (item.Length < maxPrefix)
+                    maxPrefix = item.Length;
+            }
+
+            if (maxPrefix > 0)
+            {
+                for (var i = 0; i < maxPrefix; i++)
+                {
+                    var symbol = source[0][i];
+
+                    if (source.All(x => x[i] == symbol))
+                        result.Append(symbol);
+                    else
+                        break;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ ProfitClicksTests/Helpers/ArrayHelpersTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using ProfitClicksHelpers.Helpers;
+using System;
+
+namespace ProfitClicksTests.Helpers
+{
+    [TestFixture]
+    public class ArrayHelpersTest
+    {
+        private readonly int[] _sampleArray = new[] { 7, -1, 1, -7, 0, -4, 3, 2, -5, -6, 8 };
+
+        #region ExistNumbersAsSum
+
+        [Test]
+        public void SuccessExistNumbersAsSum()
+        {
+            Assert.True(ArrayHelper.ExistNumbersAsSum(_sampleArray, 5));
+        }
+
+        [Test]
+        public void FailExistNumbersAsSum()
+        {
+            Assert.False(ArrayHelper.ExistNumbersAsSum(_sampleArray, 20));
+        }
+
+        [Test]
+        public void EmptyOrNullArrayExistNumbersAsSum()
+        {
+            Assert.Throws<ArgumentNullException>(delegate { ArrayHelper.ExistNumbersAsSum(null, 5); });
+            Assert.Throws<ArgumentNullException>(delegate { ArrayHelper.ExistNumbersAsSum(new int[0], 5); });
+        }
+
+        #endregion
+
+        #region MaxPrefix
+
+        [Test]
+        public void EmptyMaxPrefix()
+        {
+            var array = new[] { "qwe", "qweasd", "qwsdfsdf", "tr" };
+
+            Assert.AreEqual("", ArrayHelper.MaxPrefix(array));
+        }
+
+        [Test]
+        public void EmptyWithEmptyMaxPrefix()
+        {
+            var array = new[] { "qwe", "qweasd", "qwsdfsdf", "" };
+
+            Assert.AreEqual("", ArrayHelper.MaxPrefix(array));
+        }
+
+        [Test]
+        public void EmptyWithNullMaxPrefix()
+        {
+            var array = new[] { "qwe", "qweasd", "qwsdfsdf", null };
+
+            Assert.AreEqual("", ArrayHelper.MaxPrefix(array));
+        }
+
+        [Test]
+        public void OneLengthMaxPrefix()
+        {
+            var array = new[] { "qwe", "qeasd", "qwsdfsdf" };
+
+            Assert.AreEqual("q", ArrayHelper.MaxPrefix(array));
+        }
+
+        [Test]
+        public void TwoLengthMaxPrefix()
+        {
+            var array = new[] { "qwe", "qweasd", "qwsdfsdf" };
+
+            Assert.AreEqual("qw", ArrayHelper.MaxPrefix(array));
+        }
+
+        [Test]
+        public void EmptyOrNullArrayMaxPrefix()
+        {
+            Assert.Throws<ArgumentNullException>(delegate { ArrayHelper.MaxPrefix(null); });
+            Assert.Throws<ArgumentNullException>(delegate { ArrayHelper.MaxPrefix(new string[0]); });
+        }
+
+        #endregion
+    }
+}
